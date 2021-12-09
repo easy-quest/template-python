1395bb3087e3d3510d85eb875db79b7cc8e6a7d7
Ivan Yastrebov <esim.i2p@gmail.com>
2021-12-09 10:27:54 +0300

    GNU/Linux, Чт 09 дек 2021 10:27:54 MSK

--- mysite/spiders/main.py
+++ mysite/spiders/main.py
@@ -1,43 +1,44 @@
-import re
-import csv
-import time
-import requests
-import asyncio
-import aiohttp
-from random import choice
-from datetime import datetime
-from bs4 import BeautifulSoup as bs
-from fake_useragent import UserAgent
-
-
-def bs(text, param):
-    pass
-
-
-class Parser:
-    def __init__(self):
-        self.category_links = []
-        self.page_links = []
-        self.drink_links = []
-        self.csv_rows = []
-        self.csv_name = "test.csv"
-        self.basic_url = "https://www.myheritage.com/site-1262049512"
-        self.ua = UserAgent()
-        self.headers = {
-                "accept": "*/*",
-                "user-agent": self.ua.random
-        } 
-
-    def parse_categories(self):
-        response = requests.get(url=self.basic_url, headers=self.headers)
-        soup = bs(response.text, "lxml")
-        nav = soup.find("ul", class_="sidebar")
-        li_s = nav.find_all('li', class_="item ng-scope")
-
-        print(li_s)
-
-    def main(self):
-        pass
-
-    if __name__ == '__main__':
-        main()
+# import re
+# import csv
+# import time
+# import requests
+# import asyncio
+# import aiohttp
+# from random import choice
+# from datetime import datetime
+# from bs4 import BeautifulSoup as bs
+# from fake_useragent import UserAgent
+#
+#
+# def bs(text, param):
+#     pass
+#
+#
+# class Parser:
+#     def __init__(self):
+#         self.category_links = []
+#         self.page_links = []
+#         self.drink_links = []
+#         self.csv_rows = []
+#         self.csv_name = "test.csv"
+#         self.basic_url = "https://www.myheritage.com/site-1262049512"
+#         self.ua = UserAgent()
+#         self.headers = {
+#                 "accept": "*/*",
+#                 "user-agent": self.ua.random
+#         }
+#
+#     def parse_categories(self):
+#         response = requests.get(url=self.basic_url, headers=self.headers)
+#         soup = bs(response.text, "lxml")
+#         nav = soup.find("ul", class_="sidebar")
+#         li_s = nav.find_all('li', class_="item ng-scope")
+#         for index [0,1,2,3,4,5,6]:
+#             a = li_s[index].find("a")
+#             href = a["href"]
+#             link = f'{self.basic_url}[href]'
+#             self.category_links.append(link)
+#
+#
+#     def create_page_links(self):
+#
